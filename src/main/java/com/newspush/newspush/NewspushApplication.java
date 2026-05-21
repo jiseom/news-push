568df9149744589157bebac339cc8dab196a3837
지선 최 <paper138@naver.com>
2026-05-21 18:23:12 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/newspush/newspush/NewspushApplication.java
@@ -0,0 +1,13 @@
+package com.newspush.newspush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newspush/newspush/NewspushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newspush.newspush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
